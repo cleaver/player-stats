b4719dc6b5a2468beb44bc6d29c8061e079eb508
Cleaver Barnes <cbarnes@cleaver.ca>
2021-06-05 22:55:31 -0400

    configure purge only on production

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  purge: {
-    enabled: false,
-    content: [
-      '../**/*.html.eex',
-      '../**/*.html.leex',
-      '../**/views/**/*.ex',
-      '../**/live/**/*.ex',
-      './js/**/*.js',
-    ],
-  },
+  purge: [
+    '../**/*.html.eex',
+    '../**/*.html.leex',
+    '../**/views/**/*.ex',
+    '../**/live/**/*.ex',
+    './js/**/*.js',
+  ],
   darkMode: 'class', // false or 'media' or 'class'
   theme: {
     extend: {},
