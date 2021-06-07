5f24ff2ce9f59704e99214f0b5faf0dd72a29d66
Cleaver Barnes <cbarnes@cleaver.ca>
2021-06-07 12:11:29 -0400

    make sort links aware of other query params

--- lib/player_stats_web/views/rushing_stats_view.ex
+++ lib/player_stats_web/views/rushing_stats_view.ex
@@ -2,14 +2,26 @@ defmodule PlayerStatsWeb.RushingStatsView do
   use PlayerStatsWeb, :view
   import Phoenix.HTML.Link
 
-  def sort_link(conn, text, column, title, sort_params \\ {}) do
+  @doc """
+  Compose a sorting link with query string.
+
+  TODO: move sanitized `sort_params` back into `params` to save a function argument.
+  """
+  def sort_link(conn, text, column, title, params, sort_params \\ {}) do
+    query_string = Map.merge(params, %{"sort" => "asc:" <> Atom.to_string(column)})
+
     link(text <> sort_symbol(column, sort_params),
-      to: Routes.rushing_stats_path(conn, :index, sort: "asc:" <> Atom.to_string(column)),
+      to: Routes.rushing_stats_path(conn, :index, query_string),
       title: title,
       class: nil
     )
   end
 
+  @doc """
+  Add an indicator character if column is sorted.
+
+  TODO: This would be more flexible as a CSS class.
+  """
   def sort_symbol(column, sort_params) do
     case sort_params do
       {:asc, ^column} ->
